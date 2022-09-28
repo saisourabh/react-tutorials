ee4c5113dd7f75c049ad85f3cebaca4b1b810dba
saisourabh <sai.sourabh2@gmail.com>
2022-09-28 12:21:47 +0530

    #Day2 Test

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and Sourabh Tesing something to reload.
+          This is something <code>src/App.js</code> I always want to learn.
         </p>
         <a
           className="App-link"
