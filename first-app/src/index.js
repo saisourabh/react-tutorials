dacc48b691a210accff7f395390b90c5685a137f
saisourabh <sai.sourabh2@gmail.com>
2022-09-28 14:39:01 +0530

    Day 2 babel Convertion

--- first-app/src/index.js
+++ first-app/src/index.js
@@ -8,10 +8,22 @@ import ReactDOM from 'react-dom';
  }
  const Apple= function(){
     return (
-        <div>There Hi</div>
+        <div>There Hi
+          <ul>
+            <li>Vasamsetti</li>
+            <li>Veera</li>
+            <li>Venkata</li>
+            <li>Lakshmi</li>
+            <li>Sai</li>
+            <li>Sourabh</li>
+          </ul>
+        </div>
     )
  }
-
+// Babel Converted
+//  const Apple = function () {
+//     return /*#__PURE__*/React.createElement("div", null, "There Hi", /*#__PURE__*/React.createElement("ul", null, /*#__PURE__*/React.createElement("li", null, "Vasamsettio"), /*#__PURE__*/React.createElement("li", null, "Veera"), /*#__PURE__*/React.createElement("li", null, "Venkata"), /*#__PURE__*/React.createElement("li", null, "Lakshmi"), /*#__PURE__*/React.createElement("li", null, "Sai"), /*#__PURE__*/React.createElement("li", null, "Sourabh")));
+//   };
 
  ReactDOM.render(
     <App />,
