e27638afa28904ab5175107a6984786215975af5
saisourabh <sai.sourabh2@gmail.com>
2022-10-03 22:18:11 +0530

    #Day 4 Hemisphere App using Class

---
+++ hemisphere/src/HemisphereDisplay.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const HemisphereDisplay = () =>{
+     
+    return (
+        
+        <div>
+            Himeshpere Demo
+            
+        </div>
+    )
+}
+
+
+export default HemisphereDisplay;
